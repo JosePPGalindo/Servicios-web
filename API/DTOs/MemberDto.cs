d3b2685d482639b9de99d769a05127d515074f48
LAPTOP-90PAIAOG\josep <jose.padilla@kiubix.com>
2023-11-07 00:39:20 -0600

    Sección 8

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -1,22 +1,19 @@
-namespace API.Entities;
-public class AppUser
+namespace API.DTOs;
+public class MemberDto
 {
     public int Id { get; set; }
 
-    public byte[] PassWordHash { get; set; }
-
-    public byte[] PassWordSalt { get; set; }
     public string UserName { get; set; }
-    public DateOnly DateOfBirth { get; set; }
+    public string PhotoUrl { get; set; }
+    public int Age { get; set; }
     public string KnownAs { get; set; }
-    public DateTime Created { get; set; } = DateTime.UtcNow;
-    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public DateTime Created { get; set; }
+    public DateTime LastActive { get; set; }
     public string Gender { get; set; }
     public string Introducción { get; set; }
     public string LookingFor { get; set; }
     public string Interests { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
-    public List<Photo> Photos { get; set; } = new();
-
+    public List<PhotoDto> Photos { get; set; }
 }
